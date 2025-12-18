4b04944a59beb1deb5afd3a3a0cfe42bf03b5adf
nolvyn <245221879+nolvyn@users.noreply.github.com>
2025-12-18 00:55:16 -0800

    added neovim desktop entry

--- system/file.nix
+++ system/file.nix
@@ -11,6 +11,9 @@
       ".config/user-dirs.conf".text = "enabled=False";
 
       ".local/share/applications/yazi.desktop".source = ../applications/yazi.desktop;
+      ".local/share/applications/nvim.desktop".source = ../applications/nvim.desktop;
+      ".local/share/applications/neovim.desktop".source = ../applications/neovim.desktop;
+
     };
   };
 }
