b9084a5f9546b01c7a1771dad0bd696b6f134889
nolvyn <245221879+nolvyn@users.noreply.github.com>
2026-01-05 18:32:37 -0800

    Various small changes

--- config/hypr/hypr.nix
+++ config/hypr/hypr.nix
@@ -5,7 +5,7 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    withUWSM = true;
+    # withUWSM = true;
   };
   
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
