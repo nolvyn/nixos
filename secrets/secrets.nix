58388b4824b5f52fb4f3a54f6c8b6a16094026b3
nolvyn <245221879+nolvyn@users.noreply.github.com>
2026-04-08 15:25:11 -0700

    Disabled networkmanager and made wifi declarative using wpa_supplicant

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,10 @@
 let
   weebmachine = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPHKVO6qKd5sQXRMJ687TDt+eqKLt2cOE3LxzrjHnBSu";
   moenote = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAQk3xWsx4WS0WSrkEkRBfAgN+ZjDy3eHTFHNavS3JYu";
+  allHosts = [weebmachine moenote];
 in
 {
-  "weeb-password.age".publicKeys = [ weebmachine moenote ];
-  "root-password.age".publicKeys = [ weebmachine moenote ];
+  "weeb-password.age".publicKeys = allHosts;
+  "root-password.age".publicKeys = allHosts;
+  "wifi-secrets.age".publicKeys = allHosts;
 }
--- system/security/network.nix
+++ system/security/network.nix
@@ -1,9 +1,36 @@
 # network.nix
+{config, ...}:
 
 # For more information see https://nixos.wiki/wiki/Firewall
 {
+  age.secrets.wifi-secrets.file = ../../secrets/wifi-secrets.age;
+
   networking = {
-    networkmanager.enable = true;
+    networkmanager.enable = false;
+
+    wireless = {
+      enable = true;
+      userControlled.enable = true;
+      secretsFile = config.age.secrets.wifi-secrets.path;
+
+      networks = {
+        "ext:HOME_SSID" = {
+          pskRaw   = "ext:HOME_PSK";
+          priority = 100;
+        };
+
+        eduroam = {
+          priority = 90;
+          auth = ''
+            key_mgmt=WPA-EAP
+            eap=PEAP
+            phase2="auth=MSCHAPV2"
+            identity="ext:EDUROAM_IDENTITY"
+            password="ext:EDUROAM_PASSWORD"
+          '';
+        };
+      };
+    };
 
     nftables.enable = true;
     firewall = {
