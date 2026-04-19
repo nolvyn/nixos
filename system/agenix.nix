cd04070dc809a81c475bc3e02b5c50188d2d8d1c
nolvyn <245221879+nolvyn@users.noreply.github.com>
2026-04-18 17:05:21 -0700

    Fixed impermanence and agenix not being able to properly decrypt (pain and suffering)

--- system/agenix.nix
+++ system/agenix.nix
@@ -9,7 +9,9 @@
 {
   environment.systemPackages = [ inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default ];
 
-  age.secrets = {
+  age.identityPaths = [ "/persistent/etc/ssh/ssh_host_ed25519_key" ];
+
+ age.secrets = {
     weeb-password.file = ../secrets/weeb-password.age;
     root-password.file = ../secrets/root-password.age;
   }
--- system/impermanence.nix
+++ system/impermanence.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, lib, ... }:
 
 {
   fileSystems."/persistent".neededForBoot = true;
@@ -37,29 +32,27 @@
     after = [ "systemd-cryptsetup@cryptroot.service" ];
     before = [ "sysroot.mount" ];
     unitConfig.DefaultDependencies = "no";
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = pkgs.writeShellScript "rollback" ''
-        mkdir /btrfs_tmp
-        mount -o subvolid=5 /dev/mapper/cryptroot /btrfs_tmp
-        if [[ -e /btrfs_tmp/@ ]]; then
-          mkdir -p /btrfs_tmp/@old_roots
-          timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/@)" "+%Y-%m-%-d_%H:%M:%S")
-          mv /btrfs_tmp/@ "/btrfs_tmp/@old_roots/$timestamp"
-        fi
-        delete_subvolume_recursively() {
-          IFS=$'\n'
-          for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "/btrfs_tmp/$i"
-          done
-          btrfs subvolume delete "$1"
-        }
-        for i in $(find /btrfs_tmp/@old_roots/ -maxdepth 1 -mtime +30); do
-          delete_subvolume_recursively "$i"
+    serviceConfig.Type = "oneshot";
+    script = ''
+      mkdir /btrfs_tmp
+      mount -o subvolid=5 /dev/mapper/cryptroot /btrfs_tmp
+      if [[ -e /btrfs_tmp/@ ]]; then
+        mkdir -p /btrfs_tmp/@old_roots
+        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/@)" "+%Y-%m-%-d_%H:%M:%S")
+        mv /btrfs_tmp/@ "/btrfs_tmp/@old_roots/$timestamp"
+      fi
+      delete_subvolume_recursively() {
+        IFS=$'\n'
+        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+          delete_subvolume_recursively "/btrfs_tmp/$i"
         done
-        btrfs subvolume create /btrfs_tmp/@
-        umount /btrfs_tmp
-      '';
-    };
+        btrfs subvolume delete "$1"
+      }
+      for i in $(find /btrfs_tmp/@old_roots/ -maxdepth 1 -mtime +30); do
+        delete_subvolume_recursively "$i"
+      done
+      btrfs subvolume create /btrfs_tmp/@
+      umount /btrfs_tmp
+    '';
   };
 }
