a9221171a2a81e12550c7afc704fcbc1a94d84d8
nolvyn <245221879+nolvyn@users.noreply.github.com>
2026-04-20 19:09:12 -0700

    Made home impermanent

--- system/impermanence.nix
+++ system/impermanence.nix
@@ -2,6 +2,7 @@
 
 {
   fileSystems."/persistent".neededForBoot = true;
+  fileSystems."/home".neededForBoot = true;
 
   environment.persistence."/persistent" = {
     hideMounts = true;
@@ -24,10 +25,50 @@
       "/etc/ssh/ssh_host_ed25519_key"
       "/etc/ssh/ssh_host_ed25519_key.pub"
     ];
+
+    users.${config.user.name} = {
+      directories = [
+        # Personal
+        "Documents"
+        "Downloads"
+        "Games"
+        "gw-analysis"
+        "nixos"
+        "projects"
+        "school"
+        "torrents"
+
+        # Dotfiles
+        ".config"
+        ".dart-tool"
+        ".flutter"
+        ".ipython"
+        ".jupyter"
+        ".local"
+        ".pki"
+        ".ssh"
+        ".steam"
+        ".vscode"
+        ".zoom"
+
+        # Selective Cache
+        ".cache/mesa_shader_cache"
+        ".cache/radv_builtin_shaders"
+        ".cache/sleepy-launcher"
+        ".cache/spotify"
+        ".cache/qtshadercache-x86_64-little_endian-lp64"
+        ".cache/Proton"
+        ".cache/umu"
+        ".cache/umu-protonfixes"
+      ];
+      files = [
+        ".gitconfig"
+      ];
+    };
   };
 
   boot.initrd.systemd.services.rollback = {
-    description = "Rollback btrfs root subvolume";
+    description = "Rollback btrfs root and home subvolumes";
     wantedBy = [ "initrd.target" ];
     after = [ "systemd-cryptsetup@cryptroot.service" ];
     before = [ "sysroot.mount" ];
@@ -36,11 +77,19 @@
     script = ''
       mkdir /btrfs_tmp
       mount -o subvolid=5 /dev/mapper/cryptroot /btrfs_tmp
+
       if [[ -e /btrfs_tmp/@ ]]; then
         mkdir -p /btrfs_tmp/@old_roots
         timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/@)" "+%Y-%m-%-d_%H:%M:%S")
         mv /btrfs_tmp/@ "/btrfs_tmp/@old_roots/$timestamp"
       fi
+
+      if [[ -e /btrfs_tmp/@home ]]; then
+        mkdir -p /btrfs_tmp/@old_home_roots
+        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/@home)" "+%Y-%m-%-d_%H:%M:%S")
+        mv /btrfs_tmp/@home "/btrfs_tmp/@old_home_roots/$timestamp"
+      fi
+
       delete_subvolume_recursively() {
         IFS=$'\n'
         for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
@@ -48,10 +97,17 @@
         done
         btrfs subvolume delete "$1"
       }
+
       for i in $(find /btrfs_tmp/@old_roots/ -maxdepth 1 -mtime +30); do
         delete_subvolume_recursively "$i"
       done
+
+      for i in $(find /btrfs_tmp/@old_home_roots/ -maxdepth 1 -mtime +30); do
+        delete_subvolume_recursively "$i"
+      done
+
       btrfs subvolume create /btrfs_tmp/@
+      btrfs subvolume create /btrfs_tmp/@home
       umount /btrfs_tmp
     '';
   };
